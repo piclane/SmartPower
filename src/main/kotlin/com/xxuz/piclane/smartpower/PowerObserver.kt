929f93b9d0d48eec6fe0d2997630cddca34cf5b6
Yohei Hina <yohei_hina@dwango.co.jp>
2023-02-21 20:17:32 +0900

    修正

--- src/main/kotlin/com/xxuz/piclane/smartpower/PowerObserver.kt
+++ src/main/kotlin/com/xxuz/piclane/smartpower/PowerObserver.kt
@@ -7,6 +7,7 @@ import jakarta.annotation.PreDestroy
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicReference
 
 @Service
 class PowerObserver(
@@ -20,113 +21,168 @@ class PowerObserver(
     private val deviceRbid: String,
 ) {
     companion object {
+        /** ロガー */
         private val logger = LoggerFactory.getLogger(PowerObserver::class.java)
     }
 
-    private lateinit var sk: SkStack
-
-    private lateinit var ipAddr: IpAddr
-
+    /** 通信スレッド */
     private lateinit var thread: Thread
 
-    @Volatile
-    private var stopped = false
+    /** ステータス */
+    private var status: AtomicReference<Status> = AtomicReference(Status.Ready)
 
+    /**
+     * 通信スレッド開始
+     */
     @PostConstruct
-    private fun init() {
-        SkStack.addShutdownHook(Thread {
-            stopped = true
-        })
-
-        sk = SkStack(devicePath)
-
-        logger.info("SKVER: ${sk.version()}")
-        sk.setPassword(devicePassword)
-        sk.setRouteBId(deviceRbid)
-
-        val pans = sk.activeScan(0xffffffff, 6)
-        if (pans.isEmpty()) {
-            throw RuntimeException("スマートメーターが見つかりませんでした")
-        }
-
-        val pan = pans.first()
-        sk.setRegister("S2", pan.channelHex)
-        sk.setRegister("S3", pan.panIdHex)
-        ipAddr = sk.ll64(pan.addr)
-        sk.join(ipAddr)
-
-        thread = Thread {
-            try {
-                mainLoop()
-            } catch (e: Throwable) {
-                if(stopped) {
-                    return@Thread
-                }
-                logger.error("メインループでエラーが発生しました", e)
-            }
-        }
+    private fun start() {
+        thread = CommunicationThread()
         thread.name = "PowerObserver"
         thread.start()
     }
 
+    /**
+     * 通信スレッド終了
+     */
     @PreDestroy
     private fun destroy() {
-        stopped = true
-        thread.interrupt()
+        status.compareAndSet(Status.Started, Status.Stopping)
         thread.join()
-
-        sk.close()
     }
 
-    private fun mainLoop() {
-        val eojSrc = EchoNetLiteFrame.EOJ(0x05, 0xFF, 0x01) // 送信元：管理・操作関連機器クラスグループ／コントローラクラス (Appendix-3.6.2)
-        val eojDst = EchoNetLiteFrame.EOJ(0x02, 0x88, 0x01) // 送信先：住宅・設備関連機器クラスグループ／低圧スマート電力量メータクラス (Appendix-3.3.25)
-        val frame = EchoNetLiteFrame(
-            ehd1 = 0x10, // ECHONET Lite規格であることを示す (02-3.2.1.1)
-            ehd2 = 0x81, // 電文形式 1(規定電文形式)であることを示す (02-3.2.1.2)
-            tid = 1,
-            edata = EchoNetLiteFrame.EDATA(
-                sEoj = eojSrc,
-                dEoj = eojDst,
-                esv = 0x62, // プロパティ値読み出し要求
-                op = listOf(
-                    EchoNetLiteFrame.OP(
-                        epc = 0xE7, // 瞬時電力計測値
-                    ),
-                    EchoNetLiteFrame.OP(
-                        epc = 0xE8 // 瞬時電流計測値
-                    ),
-                )
-            )
-        ).toByteBuffer()
+    private inner class CommunicationThread: Thread() {
+        /** SkStack */
+        private val sk: SkStack = SkStack(devicePath)
 
-        while(!Thread.interrupted() && !stopped) {
-            sk.sendTo(1, ipAddr, 0x0E1A, SendToSecurity.ENCRYPT, frame)
-            val rx = try {
-                sk.waitForRxUdp(10000)
-            } catch (e: SkTimeoutException) {
-                continue
-            } catch (e: SkEofException) {
-                break
+        /** スマートメーターのアドレス */
+        private lateinit var ipAddr: IpAddr
+
+        override fun run() {
+            try {
+                logger.info("初期化を開始します")
+                status.set(Status.Starting)
+
+                // 初期化
+                try {
+                    init()
+                } catch(e: Throwable) {
+                    logger.error("初期化に失敗しました", e)
+                    return
+                }
+
+                status.set(Status.Started)
+
+                SkStack.addShutdownHook(Thread {
+                    status.compareAndSet(Status.Started, Status.Stopping)
+                })
+
+                logger.info("初期化が完了しました")
+
+                // メインループ
+                try {
+                    mainLoop()
+                } catch (e: Throwable) {
+                    if(status.get() == Status.Stopping) {
+                        return
+                    }
+                    logger.error("メインループでエラーが発生しました", e)
+                }
+            } finally {
+                // 後始末
+                status.set(Status.Stopped)
+                sk.close()
+            }
+        }
+
+        /**
+         * 初期化
+         */
+        private fun init() {
+            logger.info("SKVER: ${sk.version()}")
+            sk.setPassword(devicePassword)
+            sk.setRouteBId(deviceRbid)
+
+            val pans = sk.activeScan(0xffffffff, 6)
+            if (pans.isEmpty()) {
+                throw RuntimeException("スマートメーターが見つかりませんでした")
             }
 
-            val rxFrame = EchoNetLiteFrame.fromHex(rx.data)
-            val rxEData = rxFrame.edata
-            if(rxEData.sEoj == eojDst && rxEData.esv == 0x72 /** プロパティ値読み出し応答 */) {
-                rxEData.op.forEach { op ->
-                    val buf = op.toByteBuffer()
-                    when(op.epc) {
-                        0xE7 -> {
-                            println("瞬時電力計測値 ${buf.int}W")
-                        }
-                        0xE8 -> {
-                            val rPhase = buf.short.toDouble() * 0.1
-                            val tPhase = buf.short.toDouble() * 0.1
-                            println("瞬時電流計測値 R相 ${rPhase}A, T相 ${tPhase}A")
+            val pan = pans.first()
+            sk.setRegister("S2", pan.channelHex)
+            sk.setRegister("S3", pan.panIdHex)
+            ipAddr = sk.ll64(pan.addr)
+            sk.join(ipAddr)
+        }
+
+        /**
+         * メインループ
+         */
+        private fun mainLoop() {
+            val eojSrc = EchoNetLiteFrame.EOJ(0x05, 0xFF, 0x01) // 送信元：管理・操作関連機器クラスグループ／コントローラクラス (Appendix-3.6.2)
+            val eojDst = EchoNetLiteFrame.EOJ(0x02, 0x88, 0x01) // 送信先：住宅・設備関連機器クラスグループ／低圧スマート電力量メータクラス (Appendix-3.3.25)
+            val frame = EchoNetLiteFrame(
+                ehd1 = 0x10, // ECHONET Lite規格であることを示す (02-3.2.1.1)
+                ehd2 = 0x81, // 電文形式 1(規定電文形式)であることを示す (02-3.2.1.2)
+                tid = 1,
+                edata = EchoNetLiteFrame.EDATA(
+                    sEoj = eojSrc,
+                    dEoj = eojDst,
+                    esv = 0x62, // プロパティ値読み出し要求
+                    op = listOf(
+                        EchoNetLiteFrame.OP(
+                            epc = 0xE7, // 瞬時電力計測値
+                        ),
+                        EchoNetLiteFrame.OP(
+                            epc = 0xE8 // 瞬時電流計測値
+                        ),
+                    )
+                )
+            ).toByteArray()
+
+            while(status.get() != Status.Stopping) {
+                sk.sendTo(1, ipAddr, 0x0E1A, SendToSecurity.ENCRYPT, frame)
+                val rx = try {
+                    sk.waitForRxUdp(10000)
+                } catch (e: SkTimeoutException) {
+                    continue
+                } catch (e: SkEofException) {
+                    break
+                }
+
+                val rxFrame = EchoNetLiteFrame.fromHex(rx.data)
+                val rxEData = rxFrame.edata
+                if(rxEData.sEoj == eojDst && rxEData.esv == 0x72 /** プロパティ値読み出し応答 */) {
+                    rxEData.op.forEach { op ->
+                        val buf = op.toByteBuffer()
+                        when(op.epc) {
+                            0xE7 -> {
+                                println("瞬時電力計測値 ${buf.int}W")
+                            }
+                            0xE8 -> {
+                                val rPhase = buf.short.toDouble() * 0.1
+                                val tPhase = buf.short.toDouble() * 0.1
+                                println("瞬時電流計測値 R相 ${rPhase}A, T相 ${tPhase}A")
+                            }
                         }
                     }
                 }
             }
         }
+    }
+
+    /**
+     * ステータス
+     */
+    private enum class Status {
+        /** 初期化前 */
+        Ready,
+        /** 初期化中 */
+        Starting,
+        /** 開始 */
+        Started,
+        /** 停止中 */
+        Stopping,
+        /** 停止 */
+        Stopped,
     }
 }
--- src/main/kotlin/com/xxuz/piclane/smartpower/sk/SkException.kt
+++ src/main/kotlin/com/xxuz/piclane/smartpower/sk/SkException.kt
@@ -1,11 +1,21 @@
 package com.xxuz.piclane.smartpower.sk
 
-open class SkException(message: String, messageRaw: String): Exception("${message}: $messageRaw") {
-    constructor(message: String): this(message, "")
+import com.xxuz.piclane.smartpower.sk.event.EventBase
+
+open class SkException: Exception {
+    constructor(message: String, line: String): super("${message}: $line")
+    constructor(message: String, event: EventBase): super("${message}: $event")
+    constructor(message: String): super(message)
+    protected constructor(): super()
 }
 
-class SkJoinException(message: String): SkException(message)
+class SkJoinException: SkException()
 
-class SkTimeoutException(message: String): SkException(message)
+class SkTimeoutException: SkException()
 
-class SkEofException(message: String): SkException(message)
+class SkEofException: SkException()
+
+class SkIllegalResponseException: SkException {
+    constructor(message: String, line: String): super(message, line)
+    constructor(message: String): super(message)
+}
