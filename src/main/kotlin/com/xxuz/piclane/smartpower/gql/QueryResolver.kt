9f5a1ed847e779cf874278c9394d767c51ce5b88
Yohei Hina <piclane@gmail.com>
2023-02-26 23:07:51 +0900

    単相3線式に対応

--- src/main/kotlin/com/xxuz/piclane/smartpower/gql/QueryResolver.kt
+++ src/main/kotlin/com/xxuz/piclane/smartpower/gql/QueryResolver.kt
@@ -1,6 +1,7 @@
 package com.xxuz.piclane.smartpower.gql
 
 import com.xxuz.piclane.smartpower.model.Instantaneous
+import com.xxuz.piclane.smartpower.model.PowerSource
 import com.xxuz.piclane.smartpower.power.PowerObserver
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -9,8 +10,14 @@ import org.springframework.stereotype.Controller
 @Controller
 class QueryResolver(
     @Autowired
-    val observer: PowerObserver
+    private val observer: PowerObserver,
+
+    @Autowired
+    private val powerSource: PowerSource
 ) {
     @QueryMapping
     fun instantaneous(): Instantaneous = observer.getInstantaneous()
+
+    @QueryMapping
+    fun powerSource() = powerSource
 }
