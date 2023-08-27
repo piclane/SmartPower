d6779d1ad016ca0bb54a20644452f635cb6fcadf
Yohei Hina <piclane@gmail.com>
2023-08-27 21:39:57 +0900

    時計を追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {ApolloProvider} from "@apollo/client";
 import {gqlClient} from "@/DataSource";
 import {PowerMonitor} from "@/PowerMonitor";
+import {DigitalClock} from "@/DigitalClock";
 
 function App() {
 
@@ -10,6 +11,14 @@ function App() {
     <div className="App">
       <ApolloProvider client={gqlClient}>
         <PowerMonitor />
+        <DigitalClock
+            style={{
+              position: "fixed",
+              left: "1rem",
+              bottom: "1rem",
+              fontSize: "9vh",
+            }}
+        />
       </ApolloProvider>
     </div>
   );
