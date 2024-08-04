d7e593eeeb8d7a0f88f1ef0848893ef3a1f9d68b
Yohei Hina <piclane@gmail.com>
2024-08-04 21:56:06 +0900

    接続が切れた際、自動的に再接続するように修正

--- frontend/src/DataSource.ts
+++ frontend/src/DataSource.ts
@@ -3,12 +3,40 @@ import {createClient} from "graphql-ws";
 import {ApolloClient, HttpLink, InMemoryCache, split} from "@apollo/client";
 import {getMainDefinition} from "@apollo/client/utilities";
 
-const wsLink = new GraphQLWsLink(createClient({
-  url: `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/graphql`,
-}));
+// const wsLink = new GraphQLWsLink(createClient({
+//   url: `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/graphql`,
+// }));
+//
+// const httpLink = new HttpLink({
+//   uri: `${window.location.protocol}//${window.location.host}/graphql`
+// });
 
+let activeSocket: any = null;
+let timedOut: any = null;
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: `wss://power-monitor.piclane.xxuz.com/graphql`,
+  retryAttempts: Infinity,
+  shouldRetry: () => true,
+  on: {
+    connected: socket => {
+      activeSocket = socket
+    },
+    ping: (received) => {
+      if (!received)
+        // sent
+        timedOut = setTimeout(() => {
+          if (activeSocket?.readyState === WebSocket?.OPEN)
+            activeSocket?.close(4408, 'Request Timeout');
+        }, 5000); // wait 5 seconds for the pong and then close the connection
+    },
+    pong: (received) => {
+      if (received) clearTimeout(timedOut); // pong is received, clear connection close timeout
+    }
+  }
+}));
 const httpLink = new HttpLink({
-  uri: `${window.location.protocol}//${window.location.host}/graphql`
+  uri: `https://power-monitor.piclane.xxuz.com/graphql`
 });
 
 const splitLink = split(
